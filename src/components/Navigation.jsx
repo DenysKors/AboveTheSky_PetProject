08312e62227ac61f0cdd28a207a9382d87779371
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-05 15:07:34 +0200

    Header svg

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 function Navigation() {
+  const [BurgerMenuOpen, setBurgerMenuOpen] = useState(false);
   return (
-    <nav className="mx-auto flex items-center justify-between p-6">
-      <div>
-        <a href="#">AboveTheSky</a>
+    <nav className="mx-auto flex items-center justify-between p-6 lg:p-8">
+      <div className="flex ">
+        <a href="#" className="font-main p-1.5 text-base lg:text-xl">
+          AboveTheSky
+        </a>
       </div>
+      <button
+        type="button"
+        className="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        onClick={() => setBurgerMenuOpen(true)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			screens: {
-				sm: "320px",
-				md: "768px",
-				lg: "1200px",
-				xl: "1440px",
-			},
-			colors: {},
-			fontFamily: {},
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px",
+        md: "768px",
+        lg: "1200px",
+        xl: "1440px",
+      },
+      colors: {},
+      fontFamily: {
+        main: ["MadimiOne", "sans-serif"],
+      },
+      fontSize: {
+        base: "1rem",
+        xl: "1.25rem",
+      },
+    },
+  },
+  plugins: [],
 };
