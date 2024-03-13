f8f7d836980373531048e60f0ba45890c5c5b6bd
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-13 17:36:49 +0200

    Favicon, fix image path

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,6 @@
+import Astronaut from "../assets/images/astronaut.png";
+import Spaceship from "../assets/images/spaceship.png";
+
 function HeroSection() {
   return (
     <section className="relative w-full h-390 md:h-620 lg:h-720 bg-inherit bg-hero bg-contain bg-no-repeat bg-[right_-3rem_bottom] md:bg-[right_bottom]">
@@ -10,10 +13,10 @@ function HeroSection() {
         <span className="italic text-white"> - Galileo Galilei</span>
       </div>
       <div className="absolute bottom-[10px] right-[210px] w-[100px] md:bottom-[20px] md:right-[480px] md:w-[220px] lg:bottom-[30px] lg:right-[560px] lg:w-[340px]">
-        <img src="../images/spaceship.png" alt="spaceship" />
+        <img src={Spaceship} alt="spaceship" />
       </div>
       <div className="absolute bottom-[100px] left-0 w-[130px] md:bottom-[200px] md:w-[260px] lg:bottom-[240px] lg:w-[380px]">
-        <img src="./images/astronaut.png" alt="astronaut" />
+        <img src={Astronaut} alt="astronaut" />
       </div>
     </section>
   );
