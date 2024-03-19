46fd0b565b45d9dcbaa538a6498aa818c5f2291e
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-19 13:39:21 +0200

    Section size fix

--- src/components/MercurySection.jsx
+++ src/components/MercurySection.jsx
@@ -4,9 +4,9 @@ function MercurySection() {
   return (
     <section
       id="mercury"
-      className="relative w-full h-[450px] md:h-[680px] lg:h-[840px] bg-herobg"
+      className="relative w-full h-[380px] md:h-[620px] lg:h-[840px] bg-herobg"
     >
-      <div className="absolute z-0 top-[50%] right-[5%] translate-y-[-50%] w-[calc(100vw-55%)] flex justify-end">
+      <div className="absolute z-0 top-[50%] right-[5%] translate-y-[-50%] w-[calc(100vw-48%)] flex justify-end">
         <img src={Mercury} alt="mercury" />
       </div>
       <div className="absolute z-10 left-[10%] top-[15%] p-1 max-w-[180px] md:max-w-[225px] lg:max-w-[320px] xl:max-w-[380px]">
@@ -14,8 +14,7 @@ function MercurySection() {
         <p className="section-text">
           The closest planet to the Sun and the smallest planet in the Solar
           System. It is barely bigger than our Moon. Mercury's sun-facing side
-          is scorched by temperatures of around 430&#8451;, hot enough to melt
-          lead.
+          is scorched by temperatures of around 430&#8451;.
         </p>
       </div>
     </section>
--- src/components/SunSection.jsx
+++ src/components/SunSection.jsx
@@ -4,7 +4,7 @@ function SunSection() {
   return (
     <section
       id="sun"
-      className="relative w-full h-[450px] md:h-[680px] lg:h-[840px] bg-herobg"
+      className="relative w-full h-[420px] md:h-[680px] lg:h-[840px] bg-herobg"
     >
       <div className="relative h-full max-w-[1335px] bg-[radial-gradient(circle_at_30%_50%,rgba(210,37,0,1.0)_0%,rgba(210,37,0,1.0)_30%,rgb(6,6,6)_38%)]">
         <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-50%)]">
