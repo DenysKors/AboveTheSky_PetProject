9548485dfa007c92b868ec809e51b988abe4431f
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-22 13:21:01 +0200

    Sun image replace

--- src/components/SunSection.jsx
+++ src/components/SunSection.jsx
@@ -8,7 +8,7 @@ function SunSection() {
       id="sun"
       className="relative w-full h-[420px] md:h-[680px] lg:h-[840px] bg-herobg"
     >
-      <div className="relative h-full max-w-[1335px] bg-[radial-gradient(circle_at_30%_50%,rgba(210,37,0,1.0)_0%,rgba(210,37,0,1.0)_30%,rgb(6,6,6)_38%)]">
+      <div className="relative h-full max-w-[1335px] bg-[radial-gradient(circle_at_30.5%_50%,rgba(210,37,0,1.0)_0%,rgba(210,37,0,1.0)_30%,rgb(6,6,6)_38%)]">
         <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-50%)]">
           <img src={Sun} alt="sun" />
         </div>
