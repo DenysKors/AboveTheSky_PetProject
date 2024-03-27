dd7545d131aa9ca37ed7a8557c83b7fe3f736ea3
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-27 15:43:07 +0200

    Earth section text

--- src/components/EarthSection.jsx
+++ src/components/EarthSection.jsx
@@ -1,8 +1,25 @@
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
 function VenusSection() {
   return (
     <section id="earth" className="bg-herobg">
-      <div className="flex flex-col gap-3 md:flex-row justify-center items-center">
-        <div className="relative pt-[55%] border-gray-300 border-2 rounded  w-[calc(100vw-10%)]">
+      <div className="relative w-full h-[380px] md:h-[620px] lg:h-[840px]">
+        {/* <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-50%)]">
+        <img className="animate-rotate" src={Venus} alt="venus" />
+      </div> */}
+        <div className="absolute z-10 left-[10%] top-[15%] p-1 max-w-[180px] md:max-w-[225px] lg:max-w-[320px] xl:max-w-[380px]">
+          <h2 className="section-title">Earth</h2>
+          <ArrowRightIcon className="text-yellow-500 hidden md:block md:w-5 md:h-5 lg:w-7 lg:h-7 animate-pulse" />
+          <p className="section-text">
+            Earth has a moon and an atmosphere that consists of 78% nitrogen,
+            21% oxygen, and 1% other gases. It's the only place we know of
+            inhabited by living things. Beautiful nature and the perfect place
+            for life.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 md:gap-4 md:flex-row justify-center items-center">
+        <div className="relative pt-[55%] w-[calc(100vw-10%)] md:w-[calc(100vw-54%)]">
           <iframe
             src="https://player.vimeo.com/video/927491728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
             frameBorder="0"
@@ -17,7 +34,7 @@ function VenusSection() {
             title="Earth dark side"
           ></iframe>
         </div>
-        <div className="relative pt-[55%] border-gray-300 border-2 rounded  w-[calc(100vw-10%)]">
+        <div className="relative pt-[55%] w-[calc(100vw-10%)] md:w-[calc(100vw-54%)]">
           <iframe
             src="https://player.vimeo.com/video/927511584?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
             frameBorder="0"
@@ -33,24 +50,6 @@ function VenusSection() {
           ></iframe>
         </div>
       </div>
-      {/* <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-50%)]">
-        <iframe
-          src="https://player.vimeo.com/video/927491728?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          style="width:100%;height:100%;"
-          title="Earth dark side"
-        ></iframe>
-      </div> */}
-      {/* <div className="absolute z-10 right-[10%] top-[15%] p-1 max-w-[180px] md:max-w-[225px] lg:max-w-[320px] xl:max-w-[380px]">
-          <h2 className="section-title">Venus</h2>
-          <ArrowLeftIcon className="text-yellow-500 hidden md:block md:w-5 md:h-5 lg:w-7 lg:h-7 animate-pulse" />
-          <p className="section-text">
-            The hottest and brightest planet in the solar system. Similar in
-            structure and size to Earth, Venus spins slowly in the opposite
-            direction from most planets. One Venusian day takes 243 Earth days.
-          </p>
-        </div> */}
     </section>
   );
 }
