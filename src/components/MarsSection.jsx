f25f64c00968b31cd694eeae1987973945035efe
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-03-29 12:46:35 +0200

    Mars section styles

--- src/components/MarsSection.jsx
+++ src/components/MarsSection.jsx
@@ -1,6 +1,6 @@
 import { ChevronDoubleLeftIcon } from "@heroicons/react/24/outline";
 
-import Venus from "../assets/images/venus.png";
+import Mars from "../assets/images/mars.png";
 
 function MarsSection() {
   return (
@@ -8,8 +8,8 @@ function MarsSection() {
       id="mars"
       className="relative w-full h-[380px] md:h-[620px] lg:h-[840px] bg-herobg"
     >
-      <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-50%)]">
-        <img className="animate-rotate" src={Venus} alt="mars" />
+      <div className="absolute z-0 top-[50%] left-[5%] translate-y-[-50%] w-[calc(100vw-48%)]">
+        <img src={Mars} alt="mars" />
       </div>
       <div className="absolute z-10 right-[10%] top-[15%] p-1 max-w-[180px] md:max-w-[225px] lg:max-w-[320px] xl:max-w-[380px]">
         <h2 className="section-title">Mars</h2>
@@ -19,9 +19,10 @@ function MarsSection() {
           <ChevronDoubleLeftIcon className="text-yellow-500 md:w-5 md:h-5 lg:w-7 lg:h-7 animate-pulse" />
         </div>
         <p className="section-text">
-          The hottest and brightest planet in the solar system. Similar in
-          structure and size to Earth, Venus spins slowly in the opposite
-          direction from most planets. One Venusian day takes 243 Earth days.
+          Its the only planet inhabited entirely by robots and home to the
+          highest mountain in our solar system &ndash; Olympus Mons. Mars is
+          also known as the &lsquo;Red Planet&rsquo;, this colour comes from the
+          large amount of a iron oxide.
         </p>
       </div>
     </section>
