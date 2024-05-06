b1e78128c61d6dae1a6fd01e9cba54dde478e18d
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-05-06 13:53:06 +0300

    Footer copyrights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 function Footer() {
   return (
-    <footer className="mt-16 md:mt-28 lg:mt-36 h-[60vh]">
+    <footer className="relative mt-16 md:mt-28 lg:mt-36 h-[60vh]">
       <div className="w-[75vw] h-full bg-footerMoon bg-no-repeat bg-left-bottom bg-contain"></div>
+      <p className="absolute bottom-0 left-[50%] translate-x-[-50%] mb-2 text-text font-text text-xs md:text-sm lg:text-base">
+        &copy; 2023 DenKors
+      </p>
     </footer>
   );
 }
