16d70cae635939740f389319ab003d6a525e72c1
DenysKors <105918404+DenysKors@users.noreply.github.com>
2024-05-20 14:34:57 +0300

    Scroll btn before&after elements styles

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,7 +2,7 @@ function ScrollToTop() {
   return (
     <button
       type="button"
-      className="fixed bottom-10 right-[2%] w-10 h-14 z-20 bg-scrollBtn bg-transparent bg-no-repeat bg-contain"
+      className="scroll-btn bottom-10 right-[2%] w-10 lg:w-12 h-14 lg:h-16 z-20 bg-scrollBtn bg-transparent bg-no-repeat bg-contain bg-center"
       aria-label="Scroll to top button"
     ></button>
   );
